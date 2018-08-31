0e1a414b7f830b5735de43d06525afb1bebdfe58
koala0521 <328762691@qq.com>
2018-08-31 20:43:05 +0800

    sdk 版本更新

---
+++ time-clock/src/sdk/1.2.1.1/statistics.config.js
@@ -0,0 +1,4 @@
+export default {
+  app_key : 'c20e55fe9f6a51e8a9614dcce4991ef8'   //请在此行填写统计平台生产的 app_id
+}
+//  c20e55fe9f6a51e8a9614dcce4991ef8
---
+++ time-clock/src/sdk/1.2.2.1/statistics.config.js
@@ -0,0 +1,3 @@
+export default {
+  'app_key' : 'c20e55fe9f6a51e8a9614dcce4991ef8'
+}
